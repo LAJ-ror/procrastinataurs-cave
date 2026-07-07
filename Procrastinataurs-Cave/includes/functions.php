c9468feadeab269e2173cd2e83b5784480cc6292
LAJ-ror <procrastinataursror@gmail.com>
2026-07-07 10:25:28 +0800

    Update functions.php

--- Procrastinataurs-Cave/includes/functions.php
+++ Procrastinataurs-Cave/includes/functions.php
@@ -16,4 +16,15 @@ function isLoggedIn()
     return isset($_SESSION['user_id']);
 }
 
+function recordAudit($conn, $userType, $userId, $activity)
+{
+    $activity = mysqli_real_escape_string($conn, $activity);
+
+    mysqli_query(
+        $conn,
+        "INSERT INTO audit_logs(user_type, user_id, activity)
+         VALUES('$userType','$userId','$activity')"
+    );
+}
+
 ?>
