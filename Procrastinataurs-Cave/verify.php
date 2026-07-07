f16a920562e2cf7e82617a0efebf2700451485b2
LAJ-ror <procrastinataursror@gmail.com>
2026-07-07 10:25:51 +0800

    Update verify.php

---
+++ Procrastinataurs-Cave/verify.php
@@ -0,0 +1,17 @@
+<?php
+include 'includes/header.php';
+?>
+
+<div class="container mt-5">
+
+<div class="alert alert-info">
+
+Email verification is optional and will be implemented in the final version.
+
+</div>
+
+</div>
+
+<?php
+include 'includes/footer.php';
+?>
