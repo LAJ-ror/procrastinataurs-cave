1612140dc1ced1899f7a933947e2427cfed22839
Le.i <procrastinataursror@gmail.com>
2026-07-16 03:09:49 +0800

    Update login.php

--- Procrastinataurs-Cave/login.php
+++ Procrastinataurs-Cave/login.php
@@ -10,7 +10,7 @@ $remembered_email = $_COOKIE['remember_email'] ?? '';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-    $email = sanitize($_POST['email']);
+    $email = $_POST['email'];
     $password = $_POST['password'];
 
     $result = mysqli_query(
@@ -75,7 +75,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
