c2e380f3cef23e0ae50542564e1e65018def0260
Le.i <procrastinataursror@gmail.com>
2026-07-16 01:40:18 +0800

    Update login.php

--- Procrastinataurs-Cave/login.php
+++ Procrastinataurs-Cave/login.php
@@ -22,7 +22,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         $row = mysqli_fetch_assoc($result);
 
-        if (password_verify($password, $row['password'])) {
+        if ($password === $row['password']) {
 
             $_SESSION['user_id'] = $row['user_id'];
             $_SESSION['first_name'] = $row['first_name'];
