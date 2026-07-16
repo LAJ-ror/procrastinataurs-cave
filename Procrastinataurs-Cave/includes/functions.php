404d5163e019669313b6e880f77f0c736b9d4ae0
LAJ-ror <procrastinataursror@gmail.com>
2026-07-16 15:25:56 +0800

    Update functions.php

--- Procrastinataurs-Cave/includes/functions.php
+++ Procrastinataurs-Cave/includes/functions.php
@@ -16,17 +16,15 @@ function isLoggedIn()
     return isset($_SESSION['user_id']);
 }
 
-function recordAudit($conn, $userType, $userId, $activity, $details = null)
+function recordAudit($conn, $userType, $userId, $activity)
 {
     $userType = mysqli_real_escape_string($conn, $userType);
     $activity = mysqli_real_escape_string($conn, $activity);
-    $details = $details !== null ? mysqli_real_escape_string($conn, $details) : null;
 
-    $details_sql = is_null($details) || $details === '' ? "NULL" : "'" . $details . "'";
-
-    $sql = "INSERT INTO audit_logs (user_type, user_id, activity, details)
-            VALUES ('$userType', '$userId', '$activity', $details_sql)";
+    $sql = "INSERT INTO audit_logs
+            (user_type, user_id, activity)
+            VALUES
+            ('$userType', '$userId', '$activity')";
 
     mysqli_query($conn, $sql);
 }
-?>
